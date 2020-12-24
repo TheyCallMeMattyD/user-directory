6198c376acb09c3539a4fc46deab72dcf94e0999
TheMattyDGWU <themattydgwu@gmail.com>
2020-12-24 13:19:07 -0500

    global variable for employee count from API request

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ function Header () {
     return (
       <div className="header">
         <h1>Employee Directory</h1>
-        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+        <p>Use search boxes for a direct result, or sort each column by arrows.</p>
+        <p>Current Employee Directory size = {window.employeeCount} Employees</p>
       </div>
     )
   }
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+var employeeCount = 10;
 
 export default {
   // Populates Employees for the Directory
   getUsers: function() {
-    return axios.get("https://randomuser.me/api/?results=5&nat=us");
+    return axios.get("https://randomuser.me/api/?results=" + employeeCount + "&nat=us");
   }
 };
