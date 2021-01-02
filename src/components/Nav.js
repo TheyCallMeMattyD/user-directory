854f3f2095038eb9905571022e8276708026c477
TheMattyDGWU <themattydgwu@gmail.com>
2021-01-02 11:11:11 -0500

    resolve DOB search

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,5 @@
 import React from "react";
 import SearchName from "./SearchName.js";
-// import SearchDOB from "./SearchDOB.js";
 import "../styles/Nav.css";
 
 function Nav() {
@@ -18,12 +17,7 @@ function Nav() {
         <span className="navbar-toggler-icon" />
       </button>
       <div className="collapse navbar-collapse row" id="navbarNav">
-        <div className="search-area col-4">
           <SearchName />
-        </div>        
-        <div className="input-group col-8">
-          {/* <SearchDOB /> */}
-        </div>
       </div>
     </nav>
   );
