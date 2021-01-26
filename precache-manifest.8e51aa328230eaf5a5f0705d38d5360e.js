c22ed00cdc907eea68e7bfc53cd7a778bb7a26cf
TheMattyDGWU <themattydgwu@gmail.com>
2021-01-25 20:52:47 -0500

    Updates

---
+++ precache-manifest.8e51aa328230eaf5a5f0705d38d5360e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a0b19fbf80ccca2f99b4c702fd75b0",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "b8902ca76636f27ecc09",
+    "url": "/user-directory/static/css/main.af5fe01e.chunk.css"
+  },
+  {
+    "revision": "64861fa329531bc2de3c",
+    "url": "/user-directory/static/js/2.b88e2772.chunk.js"
+  },
+  {
+    "revision": "b8902ca76636f27ecc09",
+    "url": "/user-directory/static/js/main.75c9587e.chunk.js"
+  },
+  {
+    "revision": "909b53cf096af866002c",
+    "url": "/user-directory/static/js/runtime-main.ef317a52.js"
+  }
+]);
