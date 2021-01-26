359a851cc8f2de31aad94d6b482e1f6a2fe5ebe6
TheMattyDGWU <themattydgwu@gmail.com>
2021-01-25 20:36:30 -0500

    refactoring

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,6 @@ import axios from "axios";
 var employeeCount = 1000;
 
 export default {
-  // Populates Employees for the Directory
   getUsers: function() {
     return axios.get("https://randomuser.me/api/?results=" + employeeCount + "&nat=us");
   }
