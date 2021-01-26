bfdf0d9cf4480032d083f7798dc7b4ec1fd7f328
TheMattyDGWU <themattydgwu@gmail.com>
2021-01-25 20:13:45 -0500

    favicon and fixed compile error with callback return

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -79,6 +79,7 @@ const DataArea = () => {
 
   const handleSearchChange = event => {
     const filter = event.target.value;
+    // eslint-disable-next-line array-callback-return
     const filteredList = developerState.users.filter(item => {
       let values = item.name.first.toLowerCase() + " " + item.name.last.toLowerCase();
       console.log(filter, values)
